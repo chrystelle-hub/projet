cb5a8fcbfe091d1d0bd0d4cbebeb584135f39a50
chrystelle <chrystelle.damide@gmail.com>
2019-12-16 10:55:02 +0100

    no message

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+<?php require_once("bdd_connect.php");
+require_once("header_html.php");
 
+if (isset($_POST['btn'])){
+
+}
+?>
 
             <main>
                 <article>
@@ -15,8 +21,10 @@
            
             <form method = "POST" action ="">
                 <input type = "login" name = "pseudo" placeholder = "Pseudo" required><br>
-                <textarea type = "textarea" name = "message" placeholder = "Votre message"></textarea><br>
+                <textarea id = "textarea" name = "message" placeholder = "Votre message"></textarea><br>
                 <input type = "submit" name = "btn" value = "Envoyer">
             </form>
             </main>
-            
+<?php
+require_once("footer_html.php");
+?>          
