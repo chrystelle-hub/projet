97d46bab3a4d5e6315a2ed015c558b04b293a112
chrystelle <chrystelle.damide@gmail.com>
2019-12-16 15:33:55 +0100

    ajout police icône

--- header_html.php
+++ header_html.php
@@ -3,16 +3,27 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="icomoon/icomoon.css">
     </head>
 
     <body>
 
         <header>
             <h1>Chat en ligne</h1>
+            <?php if(isset($_SESSION["login"]) && isset($_SESSION["password"])) {
+                echo '<h2>Bienvenue '.$_SESSION["login"].'!</h2>';
+                }
+            ?>
             <nav class="menu">
                     <ul>
-                        <li><a href="login_chat.php"> Connexion</a></li>
-                        <li><a href="compte_chat.php"> Inscription</a></li>
+                        <li><a href="index.php"><span class="icon icon-home"></span> Accueil</a></li>
+                        <li><a href="login_chat.php"><span class="icon icon-user"></span> Connexion</a></li>
+                        <li><a href="compte_chat.php"><span class="icon icon-user-plus"></span> Inscription</a></li>
+                        <?php if(isset($_SESSION["login"]) && isset($_SESSION["password"])) {?>
+                        <li><a href="deconnexion.php"><span class="icon icon-exit"></span> Déconnexion</a></li>
+            <?php 
+                }
+            ?>
                     </ul> 
                 </nav>
             
