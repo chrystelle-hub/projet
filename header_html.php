4ab089a4b1d92daf8059e6b655cdb356cdb075f6
chrystelle <chrystelle.damide@gmail.com>
2019-12-16 12:08:08 +0100

    ajout de connexion inscription

--- header_html.php
+++ header_html.php
@@ -9,4 +9,12 @@
 
         <header>
             <h1>Chat en ligne</h1>
+            <nav class="menu">
+                    <ul>
+                        <li><a href="login_chat.php"> Connexion</a></li>
+                        <li><a href="compte_chat.php"> Inscription</a></li>
+                    </ul> 
+                </nav>
+            
         </header>
+        <div class="clearboth"></div>
