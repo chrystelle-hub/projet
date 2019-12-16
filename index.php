ccf89602d6f1b27e41c9c0d1c65caf1c8a3d3605
chrystelle <chrystelle.damide@gmail.com>
2019-12-16 10:43:29 +0100

    html

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+        <header>
+            <h1>Chat en ligne</h1>
+        </header>
+
+            <main>
+                <article>
+                    <header>
+
+                    </header>
+                    <main>
+
+                    </main>
+                    <footer>
+
+                    </footer>
+                </article>
+           
+            <form method = "POST" action ="">
+                <input type = "login" name = "pseudo" placeholder = "Pseudo" required><br>
+                <textarea type = "textarea" name = "message" placeholder = "Votre message"></textarea><br>
+                <input type = "submit" name = "btn" value = "Envoyer">
+            </form>
+            </main>
+            
+        <div class="clearboth"></div>
+
+        <footer>
+        Copyright 2019 AFPA
+        </footer>
+
+    </body>
+
+</html>
