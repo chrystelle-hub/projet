eee0233bcfeacfdf5934f8cfdbd30a1c84c6dcc4
chrystelle <chrystelle.damide@gmail.com>
2019-12-16 10:46:39 +0100

    fichier contenant le header

---
+++ header_html.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
+        <header>
+            <h1>Chat en ligne</h1>
+        </header>
